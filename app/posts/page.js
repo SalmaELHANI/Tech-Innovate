b6bc4302b31a7a2ef6b1ff174a237d38131cdf26
SALMA EL HANI <salmaelhani7@gmail.com>
2024-02-23 10:01:51 +0100

    Initial commit

---
+++ app/posts/page.js
@@ -0,0 +1,9 @@
+export default function PostsPage(){
+    return(
+     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+         <h1 class="text-3xl font-bold underline">
+             Posts Page!
+         </h1>
+      </main>
+    );
+}
